1069ec1119ebb7fba8f58a4e58823995dc1ec92e
supra1nova <supra1nova@gmail.com>
2022-09-26 09:39:20 +0900

    #19 - 게시글 뷰 기본 골격 작성 컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/test/java/com/selfpractice/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/selfpractice/projectboard/controller/ArticleControllerTest.java
@@ -23,6 +23,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
+//    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -38,7 +39,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
@@ -50,7 +50,7 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
-                .andExpect(model().attributeExists("articles/comments"));
+                .andExpect(model().attributeExists("articleComments"));
     }
 
     @Disabled("구현 중")
